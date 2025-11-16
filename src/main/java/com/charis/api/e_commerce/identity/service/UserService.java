4a2548a1069fbe0a2f8a3ca76cb78a208e364a74
sikiru <sikirurazak1@gmail.com>
2025-11-16 16:27:13 +0100

    chore: implement create user service

--- src/main/java/com/charis/api/e_commerce/identity/service/UserService.java
+++ src/main/java/com/charis/api/e_commerce/identity/service/UserService.java
@@ -4,6 +4,7 @@ import com.charis.api.e_commerce.common.exceptions.ResourceNotFoundException;
 import com.charis.api.e_commerce.identity.domain.User;
 import com.charis.api.e_commerce.identity.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepo;
+    private final PasswordEncoder passwordEncoder;
 
     public User getUserById(UUID id) {
         return userRepo.findById(id)
@@ -21,6 +23,11 @@ public class UserService {
     public User getUserByEmail(String email) {
         return userRepo.findByEmail(email)
                 .orElseThrow(()-> new ResourceNotFoundException("User Does not Exist"));
+    }
+
+    public User createUser(User user) {
+        user.setPassword_hash(passwordEncoder.encode(user.getPassword_hash()));
+        return userRepo.save(user);
     }
 
     public boolean existsByEmail(String email) {
